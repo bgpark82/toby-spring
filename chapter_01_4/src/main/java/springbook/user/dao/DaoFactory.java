20abfe40379f9438adb6e69147cefa181f97ff0c
bgpark <bgpark82@gmail.com>
2021-07-06 20:05:42 +0900

    refactoring: 펙토리 활용

--- chapter_01_4/src/main/java/springbook/user/dao/DaoFactory.java
+++ chapter_01_4/src/main/java/springbook/user/dao/DaoFactory.java
@@ -3,8 +3,12 @@ package springbook.user.dao;
 public class DaoFactory {
 
     public UserDao userDao() {
-        ConnectionMaker connectionMaker = new DConnectionMaker();
+        ConnectionMaker connectionMaker = getConnectionMaker();
         UserDao userDao = new UserDao(connectionMaker);
         return userDao;
+    }
+
+    private DConnectionMaker getConnectionMaker() {
+        return new DConnectionMaker();
     }
 }
