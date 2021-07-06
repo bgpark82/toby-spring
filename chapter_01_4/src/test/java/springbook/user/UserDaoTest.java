1f284cf6f8c82076a858b56f70ab42f96458eb82
bgpark <bgpark82@gmail.com>
2021-07-06 20:04:15 +0900

    refactoring: 펙토리 사용

--- chapter_01_4/src/test/java/springbook/user/UserDaoTest.java
+++ chapter_01_4/src/test/java/springbook/user/UserDaoTest.java
@@ -2,6 +2,7 @@ package springbook.user;
 
 import springbook.user.dao.ConnectionMaker;
 import springbook.user.dao.DConnectionMaker;
+import springbook.user.dao.DaoFactory;
 import springbook.user.dao.UserDao;
 import springbook.user.domain.User;
 
@@ -11,8 +12,8 @@ public class UserDaoTest {
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
 
-        ConnectionMaker connectionMaker = new DConnectionMaker();
-        UserDao dao = new UserDao(connectionMaker);
+        DaoFactory factory = new DaoFactory();
+        UserDao dao = factory.userDao();
 
         User user = new User();
         user.setId("whiteship");
