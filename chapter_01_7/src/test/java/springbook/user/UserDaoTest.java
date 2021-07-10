a268743c9cb57bea8c3dc57db02c4f951a5c8de4
bgpark <bgpark82@gmail.com>
2021-07-11 01:10:50 +0900

    feat: chapter_01_7

---
+++ chapter_01_7/src/test/java/springbook/user/UserDaoTest.java
@@ -0,0 +1,35 @@
+package springbook.user;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import springbook.user.dao.ConnectionMaker;
+import springbook.user.dao.DConnectionMaker;
+import springbook.user.dao.DaoFactory;
+import springbook.user.dao.UserDao;
+import springbook.user.domain.User;
+
+import java.sql.SQLException;
+
+public class UserDaoTest {
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+
+        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+        UserDao dao = context.getBean("userDao", UserDao.class);
+
+        User user = new User();
+        user.setId("whiteship");
+        user.setName("백기선");
+        user.setPassword("married");
+
+        dao.add(user);
+
+        System.out.println(user.getId() + " 등록완료");
+
+        User user2 = dao.get(user.getId());
+        System.out.println(user2.getName());
+        System.out.println(user2.getPassword());
+
+        System.out.println(user2.getId() + "조회 성공");
+   }
+}
