fe4bf67c3c5587032c064e5042bfe520dd60ce03
bgpark <bgpark82@gmail.com>
2021-07-11 22:25:51 +0900

    feat: chapter_02_3

---
+++ chatper_02_3/src/test/java/springbook/user/UserDaoTest.java
@@ -0,0 +1,59 @@
+package springbook.user;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+import springbook.user.dao.UserDao;
+import springbook.user.domain.User;
+
+import java.sql.SQLException;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class UserDaoTest {
+
+    @Test
+    public void addAndGet() throws SQLException, ClassNotFoundException {
+        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+        UserDao dao = context.getBean("userDao", UserDao.class);
+
+        User user = new User();
+        user.setId("whiteship");
+        user.setName("백기선");
+        user.setPassword("married");
+
+        dao.add(user);
+
+        User user2 = dao.get(user.getId());
+
+        assertThat(user2.getName(), is(user.getName()));
+        assertThat(user2.getPassword(), is(user.getPassword()));
+
+        dao.delete(user.getId());
+    }
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+
+        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+        // ApplicationContext context = new AnnotationConfigApplicationContext(DataFactory.class);
+        UserDao dao = context.getBean("userDao", UserDao.class);
+
+        User user = new User();
+        user.setId("whiteship");
+        user.setName("백기선");
+        user.setPassword("married");
+
+        dao.add(user);
+
+        System.out.println(user.getId() + " 등록완료");
+
+        User user2 = dao.get(user.getId());
+        System.out.println(user2.getName());
+        System.out.println(user2.getPassword());
+
+        System.out.println(user2.getId() + "조회 성공");
+
+        dao.delete(user.getId());
+   }
+}
