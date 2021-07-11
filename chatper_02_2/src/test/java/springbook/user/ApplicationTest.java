ab8a972b1f0b0aec2151b21bdef734a0bbcb01d6
bgpark <bgpark82@gmail.com>
2021-07-11 22:18:03 +0900

    refactoring: Junit 테스트로 변경

---
+++ chatper_02_2/src/test/java/springbook/user/ApplicationTest.java
@@ -0,0 +1,10 @@
+package springbook.user;
+
+import org.junit.runner.JUnitCore;
+
+public class ApplicationTest {
+
+    public static void main(String[] args) {
+        JUnitCore.main("springbook.user.UserDaoTest");
+    }
+}
--- chatper_02_2/src/test/java/springbook/user/UserDaoTest.java
+++ chatper_02_2/src/test/java/springbook/user/UserDaoTest.java
@@ -1,5 +1,6 @@
 package springbook.user;
 
+import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 import springbook.user.dao.UserDao;
@@ -7,7 +8,30 @@ import springbook.user.domain.User;
 
 import java.sql.SQLException;
 
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
 public class UserDaoTest {
+
+    @Test
+    public void addAndGet() throws SQLException, ClassNotFoundException {
+        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+        UserDao dao = context.getBean("userDao", UserDao.class);
+
+        User user = new User();
+        user.setId("whiteship");
+        user.setName("백기선");
+        user.setPassword("married");
+
+        dao.add(user);
+
+        User user2 = dao.get(user.getId());
+
+        assertThat(user2.getName(), is(user.getName()));
+        assertThat(user2.getPassword(), is(user.getPassword()));
+
+        dao.delete(user.getId());
+    }
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
 
