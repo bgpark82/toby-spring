750acd05fb6c2ad2aa239360311eaba8303de324
bgpark <bgpark82@gmail.com>
2021-07-11 22:36:07 +0900

    refactoring: 테스트 결과의 일관성

--- chatper_02_3/src/main/java/springbook/user/dao/UserDao.java
+++ chatper_02_3/src/main/java/springbook/user/dao/UserDao.java
@@ -50,17 +50,28 @@ public class UserDao  {
         return user;
     }
 
-    public void delete(String id) throws SQLException, ClassNotFoundException {
+    public void deleteAll() throws SQLException, ClassNotFoundException {
         Connection c = dataSource.getConnection();
 
-        PreparedStatement ps = c.prepareStatement("delete from users where id = ?");
-        ps.setString(1, id);
-
-        int deleted = ps.executeUpdate();
-
-        System.out.println("삭제된 행 : " + deleted);
+        PreparedStatement ps = c.prepareStatement("delete from users");
+        ps.executeUpdate();
 
         ps.close();
         c.close();
+    }
+
+    public int getCount() throws SQLException {
+        Connection c = dataSource.getConnection();
+
+        PreparedStatement ps = c.prepareStatement("select count(*) from users");
+        ResultSet rs = ps.executeQuery();
+        rs.next();
+        int count = rs.getInt(1);
+
+        rs.close();
+        ps.close();
+        c.close();
+
+        return count;
     }
 }
--- chatper_02_3/src/test/java/springbook/user/UserDaoTest.java
+++ chatper_02_3/src/test/java/springbook/user/UserDaoTest.java
@@ -18,42 +18,22 @@ public class UserDaoTest {
         ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
         UserDao dao = context.getBean("userDao", UserDao.class);
 
+        dao.deleteAll();
+        assertThat(dao.getCount(),is(0));
+
         User user = new User();
         user.setId("whiteship");
         user.setName("백기선");
         user.setPassword("married");
 
         dao.add(user);
+        assertThat(dao.getCount(),is(1));
 
         User user2 = dao.get(user.getId());
 
         assertThat(user2.getName(), is(user.getName()));
         assertThat(user2.getPassword(), is(user.getPassword()));
-
-        dao.delete(user.getId());
     }
 
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
 
-        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
-        // ApplicationContext context = new AnnotationConfigApplicationContext(DataFactory.class);
-        UserDao dao = context.getBean("userDao", UserDao.class);
-
-        User user = new User();
-        user.setId("whiteship");
-        user.setName("백기선");
-        user.setPassword("married");
-
-        dao.add(user);
-
-        System.out.println(user.getId() + " 등록완료");
-
-        User user2 = dao.get(user.getId());
-        System.out.println(user2.getName());
-        System.out.println(user2.getPassword());
-
-        System.out.println(user2.getId() + "조회 성공");
-
-        dao.delete(user.getId());
-   }
 }
