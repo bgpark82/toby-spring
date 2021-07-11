129d0808ce9d16aa51bf6abeb81534e4cf9d0737
bgpark <bgpark82@gmail.com>
2021-07-11 16:59:02 +0900

    refactoring: setter 메소드를 이용한 의존관계 주입

--- chapter_01_8/src/main/java/springbook/user/dao/UserDao.java
+++ chapter_01_8/src/main/java/springbook/user/dao/UserDao.java
@@ -9,9 +9,9 @@ import java.sql.SQLException;
 
 public class UserDao  {
 
-    private final ConnectionMaker connectionMaker;
+    private ConnectionMaker connectionMaker;
 
-    public UserDao(ConnectionMaker connectionMaker) {
+    public void setConnectionMaker(ConnectionMaker connectionMaker) {
         this.connectionMaker = connectionMaker;
     }
 
