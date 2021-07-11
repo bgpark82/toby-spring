2c5d77d42c4d5dd154b99d8c116636fe893b79de
bgpark <bgpark82@gmail.com>
2021-07-11 17:32:13 +0900

    Revert "refactoring: XML을 이용한 어플리케이션 컨텍스트"
    This reverts commit 0c2031a8

--- chapter_01_8/src/test/java/springbook/user/UserDaoTest.java
+++ chapter_01_8/src/test/java/springbook/user/UserDaoTest.java
@@ -2,6 +2,7 @@ package springbook.user;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 import springbook.user.dao.*;
 import springbook.user.domain.User;
 
@@ -11,7 +12,7 @@ public class UserDaoTest {
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
 
-        ApplicationContext context = new AnnotationConfigApplicationContext(CountingDaoFactory.class);
+        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
         UserDao dao = context.getBean("userDao", UserDao.class);
 
         CountingConnectionMaker ccm = context.getBean("connectionMaker", CountingConnectionMaker.class);
