69a65b01c73ba0935906f5f4351c77f1312f797f
bgpark <bgpark82@gmail.com>
2021-07-12 00:11:18 +0900

    refactoring: 스프링 테스트 컨텍스트 프레임워크 적용

--- chatper_02_4/src/test/java/springbook/user/UserDaoTest.java
+++ chatper_02_4/src/test/java/springbook/user/UserDaoTest.java
@@ -2,8 +2,11 @@ package springbook.user;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.GenericXmlApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import springbook.user.dao.UserDao;
 import springbook.user.domain.User;
 
@@ -12,7 +15,12 @@ import java.sql.SQLException;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations="/applicationContext.xml")
 public class UserDaoTest {
+
+    @Autowired
+    private ApplicationContext context;
 
     private UserDao dao;
     private User user1;
@@ -21,7 +29,8 @@ public class UserDaoTest {
 
     @Before
     public void setUp() {
-        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+        System.out.println(this.context);
+        System.out.println(this);
         dao = context.getBean("userDao", UserDao.class);
         user1 = new User("gyumee", "박성철", "springno1");
         user2 = new User("leegw700", "이길원", "springno2");
