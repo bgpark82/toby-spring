18f4bf22a1dbfb7c125ddf0d2c2470c78c1165fb
bgpark <bgpark82@gmail.com>
2021-07-11 16:42:37 +0900

    refactoring: 의존관계 주입 응용

--- chapter_01_7/src/test/java/springbook/user/UserDaoTest.java
+++ chapter_01_7/src/test/java/springbook/user/UserDaoTest.java
@@ -2,10 +2,7 @@ package springbook.user;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import springbook.user.dao.ConnectionMaker;
-import springbook.user.dao.DConnectionMaker;
-import springbook.user.dao.DaoFactory;
-import springbook.user.dao.UserDao;
+import springbook.user.dao.*;
 import springbook.user.domain.User;
 
 import java.sql.SQLException;
@@ -14,8 +11,11 @@ public class UserDaoTest {
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
 
-        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+        ApplicationContext context = new AnnotationConfigApplicationContext(CountingDaoFactory.class);
         UserDao dao = context.getBean("userDao", UserDao.class);
+
+        CountingConnectionMaker ccm = context.getBean("connectionMaker", CountingConnectionMaker.class);
+        System.out.println("Connection Counter : " + ccm.getCounter());
 
         User user = new User();
         user.setId("whiteship");
