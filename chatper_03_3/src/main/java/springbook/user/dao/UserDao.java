30490da602e07ae239cc9da908f719e31825edcc
bgpark <bgpark82@gmail.com>
2021-07-13 21:54:48 +0900

    refactoring: 로컬 클래스로 변경

--- chatper_03_3/src/main/java/springbook/user/dao/UserDao.java
+++ chatper_03_3/src/main/java/springbook/user/dao/UserDao.java
@@ -17,6 +17,25 @@ public class UserDao  {
     }
 
     public void add(User user) throws ClassNotFoundException, SQLException {
+
+        class AddStatement implements StatementStrategy{
+
+            User user;
+
+            public AddStatement(User user) {
+                this.user = user;
+            }
+
+            @Override
+            public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
+                PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
+                ps.setString(1, user.getId());
+                ps.setString(2, user.getName());
+                ps.setString(3, user.getPassword());
+                return ps;
+            }
+        }
+
         AddStatement st = new AddStatement(user);
         this.jdbcContextWithStatementStrategy(st);
     }
