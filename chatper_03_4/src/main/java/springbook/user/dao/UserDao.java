34a1445b06d22f770078510b28aff472aa3d367d
bgpark <bgpark82@gmail.com>
2021-07-13 22:24:34 +0900

    refactoring: JdbcContext 클래스 분리

--- chatper_03_4/src/main/java/springbook/user/dao/UserDao.java
+++ chatper_03_4/src/main/java/springbook/user/dao/UserDao.java
@@ -2,7 +2,6 @@ package springbook.user.dao;
 
 import springbook.user.domain.User;
 
-import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -10,21 +9,32 @@ import java.sql.SQLException;
 
 public class UserDao  {
 
-    private DataSource dataSource;
+    private JdbcContext jdbcContext;
 
-    public void setDataSource(DataSource dataSource) {
-        this.dataSource = dataSource;
+    public void setJdbcContext(JdbcContext jdbcContext) {
+        this.jdbcContext = jdbcContext;
     }
 
     public void add(final User user) throws  SQLException {
 
-        this.jdbcContextWithStatementStrategy(new StatementStrategy() {
+        this.jdbcContext.workWithStatementStrategy(new StatementStrategy() {
             @Override
             public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
                 PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
                 ps.setString(1, user.getId());
                 ps.setString(2, user.getName());
                 ps.setString(3, user.getPassword());
+                return ps;
+            }
+        });
+    }
+
+    public void deleteAll() throws SQLException {
+
+        this.jdbcContext.workWithStatementStrategy(new StatementStrategy() {
+            @Override
+            public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
+                PreparedStatement ps = c.prepareStatement("delete from users");
                 return ps;
             }
         });
@@ -81,17 +91,6 @@ public class UserDao  {
                 }
             }
         }
-    }
-
-    public void deleteAll() throws SQLException {
-
-        jdbcContextWithStatementStrategy(new StatementStrategy() {
-            @Override
-            public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
-                PreparedStatement ps = c.prepareStatement("delete from users");
-                return ps;
-            }
-        });
     }
 
     public int getCount() throws SQLException {
