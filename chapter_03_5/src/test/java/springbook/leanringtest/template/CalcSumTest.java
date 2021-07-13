70ce2b1c3ff66063e984b04ddc431566d73ce814
bgpark <bgpark82@gmail.com>
2021-07-14 00:20:04 +0900

    test: calcSum 테스트 추가

---
+++ chapter_03_5/src/test/java/springbook/leanringtest/template/CalcSumTest.java
@@ -0,0 +1,18 @@
+package springbook.leanringtest.template;
+
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class CalcSumTest {
+
+    @Test
+    public void sumOfNumbers() throws IOException {
+        Calculator calculator = new Calculator();
+        int sum = calculator.calcSum(getClass().getResource("/numbers.txt").getPath());
+        assertThat(sum, is(10));
+    }
+}
