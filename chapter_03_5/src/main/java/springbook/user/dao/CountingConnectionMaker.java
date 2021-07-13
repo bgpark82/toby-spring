ba4177966bae4a88a7d0f81b290586f4f39837df
bgpark <bgpark82@gmail.com>
2021-07-13 23:52:09 +0900

    feat: chapter_03_5

---
+++ chapter_03_5/src/main/java/springbook/user/dao/CountingConnectionMaker.java
@@ -0,0 +1,25 @@
+package springbook.user.dao;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class CountingConnectionMaker implements ConnectionMaker{
+
+    int counter = 0;
+
+    private ConnectionMaker connectionMaker;
+
+    public CountingConnectionMaker(ConnectionMaker realConnectionMaker) {
+        this.connectionMaker = realConnectionMaker;
+    }
+
+    @Override
+    public Connection makeConnection() throws ClassNotFoundException, SQLException {
+        this.counter++;
+        return connectionMaker.makeConnection();
+    }
+
+    public int getCounter() {
+        return this.counter;
+    }
+}
