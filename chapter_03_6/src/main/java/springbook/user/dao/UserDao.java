351ab0e6a1bb97a2585a6e7d30a1f770d9e381ae
bgpark <bgpark82@gmail.com>
2021-07-14 23:44:21 +0900

    refactoring: 불필요한 코드 제거

--- chapter_03_6/src/main/java/springbook/user/dao/UserDao.java
+++ chapter_03_6/src/main/java/springbook/user/dao/UserDao.java
@@ -13,13 +13,8 @@ import java.sql.SQLException;
 
 public class UserDao  {
 
-    private JdbcContext jdbcContext;
     private DataSource dataSource;
     private JdbcTemplate jdbcTemplate;
-
-    public void setJdbcContext(JdbcContext jdbcContext) {
-        this.jdbcContext = jdbcContext;
-    }
 
     public void setDataSource(DataSource dataSource) {
         this.jdbcTemplate = new JdbcTemplate(dataSource);
@@ -32,7 +27,6 @@ public class UserDao  {
     }
 
     public void deleteAll() throws SQLException {
-        // this.jdbcTemplate.update("delete from users");
         this.jdbcTemplate.update(new PreparedStatementCreator() {
             @Override
             public PreparedStatement createPreparedStatement(Connection connection) throws SQLException {
@@ -54,34 +48,6 @@ public class UserDao  {
                         return user;
                     }
                 });
-    }
-
-    public void jdbcContextWithStatementStrategy(StatementStrategy stmt) throws SQLException {
-        Connection c = null;
-        PreparedStatement ps = null;
-
-        try {
-            c = dataSource.getConnection();
-            ps = stmt.makePreparedStatement(c);
-            ps.executeUpdate();
-        } catch (SQLException e) {
-            throw e;
-        } finally {
-            if (ps != null) {
-                try {
-                    ps.close();
-                } catch (SQLException e) {
-                    throw e;
-                }
-            }
-            if (c != null) {
-                try {
-                    c.close();
-                } catch (SQLException e) {
-                    throw e;
-                }
-            }
-        }
     }
 
     public int getCount() throws SQLException {
