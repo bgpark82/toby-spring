77c02a1100d01f5bbd08b7b3c04bd76e5e0debb5
bgpark <bgpark82@gmail.com>
2021-07-17 00:12:52 +0900

    feat: Level 필드 추가

--- chapter_05_1/src/main/resources/create.sql
+++ chapter_05_1/src/main/resources/create.sql
@@ -3,3 +3,15 @@ create table users (
     name varchar(20) not null,
     password varchar(10) not null
 )
+alter table users add column level tinyint not null;
+alter table users add column login int not null;
+alter table users add column recommend int not null;
+
+create table users (
+    id varchar(10) primary key,
+    name varchar(20) not null,
+    password varchar(10) not null,
+    level tinyint not null,
+    login int not null,
+    recommend int not null
+)
