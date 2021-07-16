2111f07de744ec5496d5935f483bdb74bad693f3
bgpark <bgpark82@gmail.com>
2021-07-16 22:35:47 +0900

    refactoring: 기술에 독립적인 UserDao 만들기, 인터페이스 적용

--- chapter_04_1/src/main/java/springbook/user/dao/UserDao.java
+++ chapter_04_1/src/main/java/springbook/user/dao/UserDao.java
@@ -1,66 +1,18 @@
 package springbook.user.dao;
 
-import org.springframework.dao.DuplicateKeyException;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.PreparedStatementCreator;
-import org.springframework.jdbc.core.RowMapper;
 import springbook.user.domain.User;
-import springbook.user.exception.DuplicateUserIdException;
 
-import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 
-public class UserDao  {
+public interface UserDao  {
 
-    private JdbcTemplate jdbcTemplate;
-    private RowMapper<User> userMapper = new RowMapper<User>() {
-        @Override
-        public User mapRow(ResultSet rs, int rowNum) throws SQLException {
-            User user = new User();
-            user.setId(rs.getString("id"));
-            user.setName(rs.getString("name"));
-            user.setPassword(rs.getString("password"));
-            return user;
-        }
-    };
+    void add(User user);
 
-    public void setDataSource(DataSource dataSource) {
-        this.jdbcTemplate = new JdbcTemplate(dataSource);
-    }
+    User get(String id);
 
-    public void add(final User user) throws DuplicateUserIdException {
-        try {
-            this.jdbcTemplate.update("insert into users(id, name, password) values(?,?,?)",
-                    user.getId(), user.getName(), user.getPassword());
-        } catch (DuplicateKeyException e) { // DataAccessException은 checked exception인 SQLException을 JDBC에서 감싼 Runtime Exception이다 (unchecked excpetion)
-            throw new DuplicateUserIdException(e);
-        }
-    }
+    List<User> getAll();
 
-    public void deleteAll() {
-        this.jdbcTemplate.update(new PreparedStatementCreator() {
-            @Override
-            public PreparedStatement createPreparedStatement(Connection connection) throws SQLException {
-                return connection.prepareStatement("delete from users");
-            }
-        });
-    }
+    void deleteAll();
 
-    public User get(String id)  {
-        return this.jdbcTemplate.queryForObject("select * from users where id = ?",
-                new Object[] {id},
-                this.userMapper);
-    }
-
-    public int getCount()  {
-        return this.jdbcTemplate.queryForInt("select count(*) from users");
-    }
-
-    public List<User> getAll() {
-        return this.jdbcTemplate.query("select * from users order by id", this.userMapper);
-    }
+    int getCount();
 }
---
+++ chapter_04_1/src/main/java/springbook/user/dao/UserDaoJdbc.java
@@ -0,0 +1,66 @@
+package springbook.user.dao;
+
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.RowMapper;
+import springbook.user.domain.User;
+import springbook.user.exception.DuplicateUserIdException;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+public class UserDaoJdbc implements UserDao{
+
+    private JdbcTemplate jdbcTemplate;
+    private RowMapper<User> userMapper = new RowMapper<User>() {
+        @Override
+        public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+            User user = new User();
+            user.setId(rs.getString("id"));
+            user.setName(rs.getString("name"));
+            user.setPassword(rs.getString("password"));
+            return user;
+        }
+    };
+
+    public void setDataSource(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    public void add(final User user) throws DuplicateUserIdException {
+        try {
+            this.jdbcTemplate.update("insert into users(id, name, password) values(?,?,?)",
+                    user.getId(), user.getName(), user.getPassword());
+        } catch (DuplicateKeyException e) { // DataAccessException은 checked exception인 SQLException을 JDBC에서 감싼 Runtime Exception이다 (unchecked excpetion)
+            throw new DuplicateUserIdException(e);
+        }
+    }
+
+    public void deleteAll() {
+        this.jdbcTemplate.update(new PreparedStatementCreator() {
+            @Override
+            public PreparedStatement createPreparedStatement(Connection connection) throws SQLException {
+                return connection.prepareStatement("delete from users");
+            }
+        });
+    }
+
+    public User get(String id)  {
+        return this.jdbcTemplate.queryForObject("select * from users where id = ?",
+                new Object[] {id},
+                this.userMapper);
+    }
+
+    public int getCount()  {
+        return this.jdbcTemplate.queryForInt("select count(*) from users");
+    }
+
+    public List<User> getAll() {
+        return this.jdbcTemplate.query("select * from users order by id", this.userMapper);
+    }
+}
