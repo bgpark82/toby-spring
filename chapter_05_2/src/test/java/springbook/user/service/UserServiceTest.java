1c56a43ceaf4743da5f849f371c873e4cea6519f
bgpark <bgpark82@gmail.com>
2021-07-17 15:39:29 +0900

    refactoring: 테스트용 클래스 생성

--- chapter_05_2/src/test/java/springbook/user/service/UserServiceTest.java
+++ chapter_05_2/src/test/java/springbook/user/service/UserServiceTest.java
@@ -82,4 +82,22 @@ public class UserServiceTest {
         assertThat(goldLevelAddedUser.getLevel(), is(Level.GOLD));
         assertThat(emptyLevelAddedUser.getLevel(), is(Level.BASIC));
     }
+
+    static class TestUserService extends UserService {
+
+        private String id;
+
+        private TestUserService(String id) {
+            this.id = id;
+        }
+
+        @Override
+        protected void upgradeLevel(User user) {
+            if (user.getId().equals(this.id)) throw new TestUserServiceException();
+            super.upgradeLevel(user);
+        }
+    }
+
+    static class TestUserServiceException extends RuntimeException {
+    }
 }
