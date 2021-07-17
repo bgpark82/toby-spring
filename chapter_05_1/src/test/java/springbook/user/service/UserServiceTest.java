6ec5faaa786c846f89f2509957438ee9c99e19f5
bgpark <bgpark82@gmail.com>
2021-07-17 13:06:18 +0900

    refactoring: 테스트 코드 개선

--- chapter_05_1/src/test/java/springbook/user/service/UserServiceTest.java
+++ chapter_05_1/src/test/java/springbook/user/service/UserServiceTest.java
@@ -47,11 +47,20 @@ public class UserServiceTest {
 
         userService.upgradeLevels();
 
-        checkLevel(users.get(0), Level.BASIC);
-        checkLevel(users.get(1), Level.SILVER);
-        checkLevel(users.get(2), Level.SILVER);
-        checkLevel(users.get(3), Level.GOLD);
-        checkLevel(users.get(4), Level.GOLD);
+        checkLevelUpgraded(users.get(0), false);
+        checkLevelUpgraded(users.get(1), true);
+        checkLevelUpgraded(users.get(2), false);
+        checkLevelUpgraded(users.get(3), true);
+        checkLevelUpgraded(users.get(4), false);
+    }
+
+    private void checkLevelUpgraded(User user, boolean upgraded) {
+        User userUpdate = userDao.get(user.getId());
+        if (upgraded) {
+            assertThat(userUpdate.getLevel(), is(user.getLevel().nextLevel()));
+        } else {
+            assertThat(userUpdate.getLevel(), is(user.getLevel()));
+        }
     }
 
     @Test
@@ -70,10 +79,5 @@ public class UserServiceTest {
 
         assertThat(goldLevelAddedUser.getLevel(), is(Level.GOLD));
         assertThat(emptyLevelAddedUser.getLevel(), is(Level.BASIC));
-    }
-
-    private void checkLevel(User user, Level expectedLevel) {
-        User userUpdated = userDao.get(user.getId());
-        assertThat(userUpdated.getLevel(), is(expectedLevel));
     }
 }
