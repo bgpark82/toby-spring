8fcd2af5181ad95b46ec788f1205bebeae91ff80
bgpark <bgpark82@gmail.com>
2021-07-17 11:22:03 +0900

    feat: upgradeLevels 메소드 추가

--- chapter_05_1/src/main/java/springbook/user/service/UserService.java
+++ chapter_05_1/src/main/java/springbook/user/service/UserService.java
@@ -1,6 +1,10 @@
 package springbook.user.service;
 
 import springbook.user.dao.UserDao;
+import springbook.user.domain.Level;
+import springbook.user.domain.User;
+
+import java.util.List;
 
 public class UserService {
 
@@ -8,5 +12,29 @@ public class UserService {
 
     public void setUserDao(UserDao userDao) {
         this.userDao = userDao;
+    }
+
+    public void upgradeLevels() {
+        List<User> users = this.userDao.getAll();
+        Boolean changed = null;
+        for (User user : users) {
+            if (user.getLevel() == Level.BASIC && user.getLogin() >= 50) {
+                user.setLevel(Level.SILVER);
+                changed = true;
+            }
+            else if (user.getLevel() == Level.SILVER && user.getRecommend() >= 30) {
+                user.setLevel(Level.GOLD);
+                changed = true;
+            }
+            else if (user.getLevel() == Level.GOLD) {
+                changed = false;
+            }
+            else {
+                changed = false;
+            }
+            if (changed) {
+                userDao.update(user);
+            }
+        }
     }
 }
