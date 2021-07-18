cf3960d4d1c326e78a9cfc5c940a85fcad6f6af3
bgpark <bgpark82@gmail.com>
2021-07-18 21:36:17 +0900

    refactoring: JavaMail을 이용한 메일 발송 메소드

--- chapter_05_4/src/main/java/springbook/user/service/UserService.java
+++ chapter_05_4/src/main/java/springbook/user/service/UserService.java
@@ -1,13 +1,22 @@
 package springbook.user.service;
 
+import com.sun.xml.internal.ws.protocol.soap.MessageCreationException;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.support.DefaultTransactionDefinition;
 import springbook.user.dao.UserDao;
 import springbook.user.domain.Level;
 import springbook.user.domain.User;
+import sun.jvm.hotspot.debugger.AddressException;
 
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import java.util.List;
+import java.util.Properties;
 
 public class UserService {
 
@@ -62,5 +71,29 @@ public class UserService {
     protected void upgradeLevel(User user) {
         user.upgradeLevel();
         userDao.update(user);
+        sendUpgradeEmail(user);
+    }
+
+    private void sendUpgradeEmail(User user) {
+        Properties props = new Properties();
+        props.put("mail.smtp.host", "mail.ksug.org");
+        Session s = Session.getInstance(props, null);
+
+        MimeMessage message = new MimeMessage(s);
+        try {
+            message.setFrom(new InternetAddress("useradmin@ksug.org"));
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(user.getEmail()));
+            message.setSubject("Upgrade 안내");
+            message.setText("사용자 등급이 " + user.getLevel().name() + "로 업그레이드 되었습니다");
+            Transport.send(message);
+        } catch (AddressException e) {
+            throw new RuntimeException(e);
+        } catch (MessageCreationException e) {
+            throw new RuntimeException(e);
+        } catch (javax.mail.internet.AddressException e) {
+            e.printStackTrace();
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
     }
 }
