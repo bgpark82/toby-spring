49a678e0d2099619589b15a132fe2edc8e79f82c
bgpark <bgpark82@gmail.com>
2021-07-19 22:41:24 +0900

    refactoring: 트랜잭션이 적용된 UserServiceTx

--- chapter_06_1/src/main/java/springbook/user/service/UserServiceTx.java
+++ chapter_06_1/src/main/java/springbook/user/service/UserServiceTx.java
@@ -1,10 +1,18 @@
 package springbook.user.service;
 
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 import springbook.user.domain.User;
 
 public class UserServiceTx implements UserService{
 
     UserService userService;
+    PlatformTransactionManager transactionManager;
+
+    public void setTransactionManager(PlatformTransactionManager transactionManager) {
+        this.transactionManager = transactionManager;
+    }
 
     public void setUserService(UserService userService) {
         this.userService = userService;
@@ -17,6 +25,13 @@ public class UserServiceTx implements UserService{
 
     @Override
     public void upgradeLevels() {
-        userService.upgradeLevels();
+        TransactionStatus status = transactionManager.getTransaction(new DefaultTransactionDefinition());
+        try {
+            userService.upgradeLevels();
+            transactionManager.commit(status);
+        } catch (RuntimeException e) {
+            transactionManager.rollback(status);
+            throw e;
+        }
     }
 }
