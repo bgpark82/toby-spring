36319411e16a65e504d119e3d42a05b8e2fa4579
bgpark <bgpark82@gmail.com>
2021-07-20 23:43:33 +0900

    test: 프록시 타겟 생성

--- chapter_06_3/src/test/java/springbook/leanringtest/jdk/ReflectionTest.java
+++ chapter_06_3/src/test/java/springbook/leanringtest/jdk/ReflectionTest.java
@@ -27,4 +27,12 @@ public class ReflectionTest {
         Method charAtMethod = String.class.getMethod("charAt", int.class);
         assertThat(charAtMethod.invoke(name, 0), is('S'));
     }
+
+    @Test
+    public void simpleProxy() {
+        Hello hello = new HelloTarget();
+        assertThat(hello.sayHello("Toby"), is("Hello Toby"));
+        assertThat(hello.sayHi("Toby"), is("Hi Toby"));
+        assertThat(hello.sayThankYou("Toby"), is("Thank You Toby"));
+    }
 }
