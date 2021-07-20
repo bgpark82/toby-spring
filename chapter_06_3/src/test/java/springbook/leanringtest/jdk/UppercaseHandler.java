4e68e88692058074467ba4f9cbe3b30e84f37e45
bgpark <bgpark82@gmail.com>
2021-07-21 00:45:52 +0900

    refactoring: 확장된 UppercaseHandler

--- chapter_06_3/src/test/java/springbook/leanringtest/jdk/UppercaseHandler.java
+++ chapter_06_3/src/test/java/springbook/leanringtest/jdk/UppercaseHandler.java
@@ -5,15 +5,20 @@ import java.lang.reflect.Method;
 
 public class UppercaseHandler implements InvocationHandler {
 
-    Hello target;
+    // 어떤 종류이 인터페이스를 구현한 타겟에도 적용가능하도록 Object 타입으로 수정
+    Object target;
 
-    public UppercaseHandler(Hello target) {
+    public UppercaseHandler(Object target) {
         this.target = target;
     }
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-        String ret = (String) method.invoke(target, args); // 메소드 호출을 타겟으로 위임
-        return ret.toUpperCase(); // 부가기능 제공
+        Object ret = method.invoke(target, args); // 메소드 호출을 타겟으로 위임
+        if (ret instanceof String) {
+            return ((String) ret).toUpperCase();
+        } else {
+            return ret; // 부가기능 제공
+        }
     }
 }
