b9b6472c9caacf481cd94096274e4a7aaf0b74b7
bgpark <bgpark82@gmail.com>
2021-07-21 00:41:24 +0900

    feat: 다이나믹 프록시 생성 테스트

--- chapter_06_3/src/test/java/springbook/leanringtest/jdk/ReflectionTest.java
+++ chapter_06_3/src/test/java/springbook/leanringtest/jdk/ReflectionTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
@@ -39,6 +40,18 @@ public class ReflectionTest {
     @Test
     public void helloUppercase() {
         HelloUppercase proxiedHello = new HelloUppercase(new HelloTarget());
+        assertThat(proxiedHello.sayHello("Toby"), is("HELLO TOBY"));
+        assertThat(proxiedHello.sayHi("Toby"), is("HI TOBY"));
+        assertThat(proxiedHello.sayThankYou("Toby"), is("THANK YOU TOBY"));
+    }
+
+    @Test
+    public void dynamicProxy() {
+        // 생성된 다이나믹 프록시 객체는 Hello 인터페이스를 구현하고 있으므로 Hello 타입으로 캐스팅해도 안전
+        Hello proxiedHello = (Hello) Proxy.newProxyInstance(
+                getClass().getClassLoader(), // 동적으로 생성되는 다이나믹 프록시 클래스의 로딩에 사용할 클래스 로더
+                new Class[]{Hello.class}, // 구현할 인터페이스
+                new UppercaseHandler(new HelloTarget())); // 부가기능과 위임코드를 담은 InvocationHandler
         assertThat(proxiedHello.sayHello("Toby"), is("HELLO TOBY"));
         assertThat(proxiedHello.sayHi("Toby"), is("HI TOBY"));
         assertThat(proxiedHello.sayThankYou("Toby"), is("THANK YOU TOBY"));
