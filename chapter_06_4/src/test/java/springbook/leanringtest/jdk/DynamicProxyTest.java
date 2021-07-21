d7e500428da2760a43cbba20d8f5c6e375203f8e
bgpark <bgpark82@gmail.com>
2021-07-22 00:45:09 +0900

    test: 스프링 ProxyFactoryBean을 이용한 다이나믹 프록시 테스트

--- chapter_06_4/src/test/java/springbook/leanringtest/jdk/DynamicProxyTest.java
+++ chapter_06_4/src/test/java/springbook/leanringtest/jdk/DynamicProxyTest.java
@@ -1,6 +1,7 @@
 package springbook.leanringtest.jdk;
 
 import org.junit.Test;
+import org.springframework.aop.framework.ProxyFactoryBean;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
@@ -10,7 +11,7 @@ import java.util.Date;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
-public class ReflectionTest {
+public class DynamicProxyTest {
 
     @Test
     public void invokeMethod() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
@@ -47,12 +48,25 @@ public class ReflectionTest {
     }
 
     @Test
-    public void dynamicProxy() {
+    public void jdkDynamicProxy() {
         // 생성된 다이나믹 프록시 객체는 Hello 인터페이스를 구현하고 있으므로 Hello 타입으로 캐스팅해도 안전
         Hello proxiedHello = (Hello) Proxy.newProxyInstance(
                 getClass().getClassLoader(), // 동적으로 생성되는 다이나믹 프록시 클래스의 로딩에 사용할 클래스 로더
                 new Class[]{Hello.class}, // 구현할 인터페이스
                 new UppercaseHandler(new HelloTarget())); // 부가기능과 위임코드를 담은 InvocationHandler
+        assertThat(proxiedHello.sayHello("Toby"), is("HELLO TOBY"));
+        assertThat(proxiedHello.sayHi("Toby"), is("HI TOBY"));
+        assertThat(proxiedHello.sayThankYou("Toby"), is("THANK YOU TOBY"));
+    }
+
+    @Test
+    public void proxyFactoryBean() {
+        ProxyFactoryBean pfBean = new ProxyFactoryBean();
+        pfBean.setTarget(new HelloTarget()); // 타겟 설정
+        pfBean.addAdvice(new UppercaseAdvice()); // 부가기능을 담은 어드바이스를 추가한다
+
+        Hello proxiedHello = (Hello) pfBean.getObject(); // FactoryBean이므로 getObject로 생성된 프록시를 가져온다
+
         assertThat(proxiedHello.sayHello("Toby"), is("HELLO TOBY"));
         assertThat(proxiedHello.sayHi("Toby"), is("HI TOBY"));
         assertThat(proxiedHello.sayThankYou("Toby"), is("THANK YOU TOBY"));
