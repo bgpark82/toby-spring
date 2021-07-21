a5e355e88812e0169dc81ca3513f5b80cd8c2890
bgpark <bgpark82@gmail.com>
2021-07-22 00:31:50 +0900

    feat: chapter_06_4

---
+++ chapter_06_4/src/test/java/springbook/leanringtest/jdk/ReflectionTest.java
@@ -0,0 +1,66 @@
+package springbook.leanringtest.jdk;
+
+import org.junit.Test;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.Date;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class ReflectionTest {
+
+    @Test
+    public void invokeMethod() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+
+        String name = "Spring";
+
+        // length()
+        assertThat(name.length(), is(6));
+
+        Method lengthMethod = String.class.getMethod("length");
+        assertThat((Integer) lengthMethod.invoke(name), is(6));
+
+        // charAt
+        assertThat(name.charAt(0), is('S'));
+
+        Method charAtMethod = String.class.getMethod("charAt", int.class);
+        assertThat(charAtMethod.invoke(name, 0), is('S'));
+    }
+
+    @Test
+    public void simpleProxy() {
+        Hello hello = new HelloTarget();
+        assertThat(hello.sayHello("Toby"), is("Hello Toby"));
+        assertThat(hello.sayHi("Toby"), is("Hi Toby"));
+        assertThat(hello.sayThankYou("Toby"), is("Thank You Toby"));
+    }
+
+    @Test
+    public void helloUppercase() {
+        HelloUppercase proxiedHello = new HelloUppercase(new HelloTarget());
+        assertThat(proxiedHello.sayHello("Toby"), is("HELLO TOBY"));
+        assertThat(proxiedHello.sayHi("Toby"), is("HI TOBY"));
+        assertThat(proxiedHello.sayThankYou("Toby"), is("THANK YOU TOBY"));
+    }
+
+    @Test
+    public void dynamicProxy() {
+        // 생성된 다이나믹 프록시 객체는 Hello 인터페이스를 구현하고 있으므로 Hello 타입으로 캐스팅해도 안전
+        Hello proxiedHello = (Hello) Proxy.newProxyInstance(
+                getClass().getClassLoader(), // 동적으로 생성되는 다이나믹 프록시 클래스의 로딩에 사용할 클래스 로더
+                new Class[]{Hello.class}, // 구현할 인터페이스
+                new UppercaseHandler(new HelloTarget())); // 부가기능과 위임코드를 담은 InvocationHandler
+        assertThat(proxiedHello.sayHello("Toby"), is("HELLO TOBY"));
+        assertThat(proxiedHello.sayHi("Toby"), is("HI TOBY"));
+        assertThat(proxiedHello.sayThankYou("Toby"), is("THANK YOU TOBY"));
+    }
+
+    @Test
+    public void newInstanceTest() throws ClassNotFoundException, IllegalAccessException, InstantiationException {
+        Date date = (Date) Class.forName("java.util.Date").newInstance();
+        System.out.println(date);
+    }
+}
