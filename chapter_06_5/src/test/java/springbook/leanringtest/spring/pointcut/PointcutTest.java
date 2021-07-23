435279632610c47323f955a694d67145516b49cc
bgpark <bgpark82@gmail.com>
2021-07-23 21:49:55 +0900

    test: 포인트컷 표현식 테스트

--- chapter_06_5/src/test/java/springbook/leanringtest/spring/pointcut/PointcutTest.java
+++ chapter_06_5/src/test/java/springbook/leanringtest/spring/pointcut/PointcutTest.java
@@ -11,6 +11,7 @@ public class PointcutTest {
     @Test
     public void name() throws NoSuchMethodException {
         System.out.println(Target.class.getMethod("minus", int.class, int.class));
+        System.out.println(Target.class.getMethod("plus", int.class, int.class));
     }
 
     @Test
@@ -26,5 +27,27 @@ public class PointcutTest {
 
         // Bean.method
         assertThat(pointcut.getClassFilter().matches(Bean.class) && pointcut.getMethodMatcher().matches(Target.class.getMethod("method"), null), is(false));
+    }
+
+
+    @Test
+    public void pointcut() throws NoSuchMethodException {
+        targetClassPointcutMatches("execution(* *(..))", true, true, true, true, true, true);
+    }
+
+    public void targetClassPointcutMatches(String expression, boolean... expected) throws NoSuchMethodException {
+        pointcutMatches(expression, expected[0], Target.class, "hello");
+        pointcutMatches(expression, expected[1], Target.class, "hello", String.class);
+        pointcutMatches(expression, expected[2], Target.class, "plus", int.class, int.class);
+        pointcutMatches(expression, expected[3], Target.class, "minus", int.class, int.class);
+        pointcutMatches(expression, expected[4], Target.class, "method");
+        pointcutMatches(expression, expected[5], Bean.class, "method");
+    }
+
+    public void pointcutMatches(String expression, Boolean expected, Class<?> clazz, String methodName, Class<?>... args) throws NoSuchMethodException {
+        AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+        pointcut.setExpression(expression);
+
+        assertThat(pointcut.getClassFilter().matches(clazz) && pointcut.getMethodMatcher().matches(clazz.getMethod(methodName, args), null), is(expected));
     }
 }
