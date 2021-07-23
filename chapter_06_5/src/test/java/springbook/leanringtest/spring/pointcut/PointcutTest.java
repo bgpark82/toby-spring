7487f2e513f8693665504525a6ed6a06fe001855
bgpark <bgpark82@gmail.com>
2021-07-23 21:35:49 +0900

    test: 메소드 시그니처를 이용한 포인트컷 표현식 테스트

---
+++ chapter_06_5/src/test/java/springbook/leanringtest/spring/pointcut/PointcutTest.java
@@ -0,0 +1,30 @@
+package springbook.leanringtest.spring.pointcut;
+
+import org.junit.Test;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class PointcutTest {
+
+    @Test
+    public void name() throws NoSuchMethodException {
+        System.out.println(Target.class.getMethod("minus", int.class, int.class));
+    }
+
+    @Test
+    public void methodSignaturePointcut() throws NoSuchMethodException {
+        AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+        pointcut.setExpression("execution(public int springbook.leanringtest.spring.pointcut.Target.minus(int,int) throws java.lang.RuntimeException)");
+
+        // Target.minus
+        assertThat(pointcut.getClassFilter().matches(Target.class) && pointcut.getMethodMatcher().matches(Target.class.getMethod("minus", int.class, int.class), null), is(true));
+
+        // Target.plus
+        assertThat(pointcut.getClassFilter().matches(Target.class) && pointcut.getMethodMatcher().matches(Target.class.getMethod("plus", int.class, int.class), null), is(false));
+
+        // Bean.method
+        assertThat(pointcut.getClassFilter().matches(Bean.class) && pointcut.getMethodMatcher().matches(Target.class.getMethod("method"), null), is(false));
+    }
+}
