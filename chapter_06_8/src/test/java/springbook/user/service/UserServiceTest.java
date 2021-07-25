245453954badb38311b99a266b656fefc74419e7
bgpark <bgpark82@gmail.com>
2021-07-25 11:17:23 +0900

    test: @Rollback 어노테이션 테스트

--- chapter_06_8/src/test/java/springbook/user/service/UserServiceTest.java
+++ chapter_06_8/src/test/java/springbook/user/service/UserServiceTest.java
@@ -9,6 +9,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.dao.TransientDataAccessResourceException;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.PlatformTransactionManager;
@@ -196,6 +197,15 @@ public class UserServiceTest {
     @Test(expected = TransientDataAccessResourceException.class)
     @Transactional(readOnly = true)
     public void transactionSyncAnnotation() {
+        userDao.deleteAll();
+        userService.add(users.get(0));
+        userService.add(users.get(1));
+    }
+
+    @Test
+    @Transactional
+    @Rollback
+    public void transactionSyncRollbackAnnotation() {
         userDao.deleteAll();
         userService.add(users.get(0));
         userService.add(users.get(1));
