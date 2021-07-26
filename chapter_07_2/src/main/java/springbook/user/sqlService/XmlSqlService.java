4690376750b98416895818abaa2c5b351263045b
bgpark <bgpark82@gmail.com>
2021-07-27 00:15:12 +0900

    refactoring: Sql 파일 이름 DI로 변경

--- chapter_07_2/src/main/java/springbook/user/sqlService/XmlSqlService.java
+++ chapter_07_2/src/main/java/springbook/user/sqlService/XmlSqlService.java
@@ -14,6 +14,11 @@ import java.util.Map;
 public class XmlSqlService implements SqlService{
 
     private Map<String, String> sqlMap = new HashMap<String, String>();
+    private String sqlmapFile;
+
+    public void setSqlmapFile(String sqlmapFile) {
+        this.sqlmapFile = sqlmapFile;
+    }
 
     public XmlSqlService() {
         String contextPath = Sqlmap.class.getPackage().getName();
@@ -21,7 +26,7 @@ public class XmlSqlService implements SqlService{
         try {
             JAXBContext context = JAXBContext.newInstance(contextPath);
             Unmarshaller unmarshaller = context.createUnmarshaller();
-            InputStream is = UserDao.class.getResourceAsStream("sqlmap.xml");
+            InputStream is = UserDao.class.getResourceAsStream(this.sqlmapFile);
             Sqlmap sqlmap = (Sqlmap) unmarshaller.unmarshal(is);
 
             for (SqlType sql : sqlmap.getSql()) {
