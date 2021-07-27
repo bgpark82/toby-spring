3ae94f0509bde979404c574afa7fcc8051c1a788
bgpark <bgpark82@gmail.com>
2021-07-28 00:42:58 +0900

    refactoring: SqlRegistry 구현

--- chapter_07_2/src/main/java/springbook/user/sqlService/XmlSqlService.java
+++ chapter_07_2/src/main/java/springbook/user/sqlService/XmlSqlService.java
@@ -12,13 +12,38 @@ import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
 
-public class XmlSqlService implements SqlService{
+public class XmlSqlService implements SqlService, SqlRegistry{
+
+    private SqlReader sqlReader;
+    private SqlRegistry sqlRegistry;
+
+    public void setSqlReader(SqlReader sqlReader) {
+        this.sqlReader = sqlReader;
+    }
+
+    public void setSqlRegistry(SqlRegistry sqlRegistry) {
+        this.sqlRegistry = sqlRegistry;
+    }
 
     private Map<String, String> sqlMap = new HashMap<String, String>();
     private String sqlmapFile;
 
     public void setSqlmapFile(String sqlmapFile) {
         this.sqlmapFile = sqlmapFile;
+    }
+
+    @Override
+    public void registerSql(String key, String sql) {
+        sqlMap.put(key, sql);
+    }
+
+    @Override
+    public String findSql(String key) throws SqlNotFoundException {
+        String sql = sqlMap.get(key);
+        if (sql == null) {
+            throw new SqlNotFoundException(key+"에 대한 SQL을 찾을 수 없습니다");
+        }
+        return sql;
     }
 
     @PostConstruct
