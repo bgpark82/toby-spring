e88153e748a2b7c157f7185870efb5605044efcd
bgpark <bgpark82@gmail.com>
2021-08-01 01:11:19 +0900

    refactoring: 트랜잭션 기능을 가진 EmbeddedDbSqlRegistry

--- chapter_07_5/src/main/java/springbook/user/sqlService/EmbeddedDbSqlRegistry.java
+++ chapter_07_5/src/main/java/springbook/user/sqlService/EmbeddedDbSqlRegistry.java
@@ -2,6 +2,10 @@ package springbook.user.sqlService;
 
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.simple.SimpleJdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import javax.sql.DataSource;
 import java.util.Map;
@@ -9,9 +13,11 @@ import java.util.Map;
 public class EmbeddedDbSqlRegistry implements UpdatableSqlRegistry{
 
     SimpleJdbcTemplate jdbc;
+    TransactionTemplate transactionTemplate;
 
     public void setDataSource(DataSource dataSource) {
         this.jdbc = new SimpleJdbcTemplate(dataSource);
+        this.transactionTemplate = new TransactionTemplate(new DataSourceTransactionManager(dataSource));
     }
 
     @Override
@@ -37,9 +43,15 @@ public class EmbeddedDbSqlRegistry implements UpdatableSqlRegistry{
     }
 
     @Override
-    public void updateSql(Map<String, String> sqlmap) throws SqlUpdateFailureException {
-        for (Map.Entry<String, String> entry : sqlmap.entrySet()) {
-            updateSql(entry.getKey(), entry.getValue());
-        }
+    public void updateSql(final Map<String, String> sqlmap) throws SqlUpdateFailureException { // 익명 내부 클래스로 만들어지는 콜백 객체 안에서 사용되는 것이라 final 필요
+        transactionTemplate.execute(new TransactionCallbackWithoutResult() {
+            // 트랜잭션 템플릿이 만든 트랜잭션 경계 안에서 동작할 코드를 콜백으로 만들고 TransactionTemplate의 execute() 메솓에 전달한다
+            @Override
+            protected void doInTransactionWithoutResult(TransactionStatus status) {
+                for (Map.Entry<String, String> entry : sqlmap.entrySet()) {
+                    updateSql(entry.getKey(), entry.getValue());
+                }
+            }
+        });
     }
 }
