5a2312305e6322621b66afedee4bd845ba37cdff
bgpark <bgpark82@gmail.com>
2021-07-31 23:52:09 +0900

    feat: 내장형 DB를 사용하는 SQL 레지스트리

--- chapter_07_5/src/main/java/springbook/user/sqlService/SqlNotFoundException.java
+++ chapter_07_5/src/main/java/springbook/user/sqlService/SqlNotFoundException.java
@@ -5,4 +5,8 @@ public class SqlNotFoundException extends RuntimeException{
     public SqlNotFoundException(String message) {
         super(message);
     }
+
+    public SqlNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
