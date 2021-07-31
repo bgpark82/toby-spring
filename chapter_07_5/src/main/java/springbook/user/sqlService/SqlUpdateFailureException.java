30343617265c28e60a38d7d5dbcf814678349b74
bgpark <bgpark82@gmail.com>
2021-07-31 21:36:19 +0900

    feat: ConcurrentHashMap을 이용한 SQL 레지스트리

--- chapter_07_5/src/main/java/springbook/user/sqlService/SqlUpdateFailureException.java
+++ chapter_07_5/src/main/java/springbook/user/sqlService/SqlUpdateFailureException.java
@@ -1,4 +1,8 @@
 package springbook.user.sqlService;
 
 public class SqlUpdateFailureException extends RuntimeException{
+
+    public SqlUpdateFailureException(String message) {
+        super(message);
+    }
 }
