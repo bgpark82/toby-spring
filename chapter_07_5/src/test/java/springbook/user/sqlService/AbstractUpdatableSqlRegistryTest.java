ceefb51cadc89048ce51990778a5a6caf5296842
bgpark <bgpark82@gmail.com>
2021-08-01 00:09:34 +0900

    refactoring: updatableSqlRegistry에 대한 테스트 추상 클래스

--- chapter_07_5/src/test/java/springbook/user/sqlService/AbstractUpdatableSqlRegistryTest.java
+++ chapter_07_5/src/test/java/springbook/user/sqlService/AbstractUpdatableSqlRegistryTest.java
@@ -9,17 +9,19 @@ import java.util.Map;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
-public class ConcurrentHashMapSqlRegistryTest {
+public abstract class AbstractUpdatableSqlRegistryTest {
 
     UpdatableSqlRegistry sqlRegistry;
 
     @Before
     public void setUp() throws Exception {
-        sqlRegistry = new ConcurrentHashMapSqlRegistry();
+        sqlRegistry = createUpdatableRegistry();
         sqlRegistry.registerSql("KEY1","SQL1");
         sqlRegistry.registerSql("KEY2","SQL2");
         sqlRegistry.registerSql("KEY3","SQL3");
     }
+
+    abstract protected UpdatableSqlRegistry createUpdatableRegistry();
 
     @Test
     public void find() {
@@ -52,7 +54,7 @@ public class ConcurrentHashMapSqlRegistryTest {
         sqlRegistry.updateSql("SQL9999!#@$","Modified2");
     }
 
-    private void checkFindResult(String expected1, String expected2, String expected3) {
+    protected void checkFindResult(String expected1, String expected2, String expected3) {
         assertThat(sqlRegistry.findSql("KEY1"), is(expected1));
         assertThat(sqlRegistry.findSql("KEY2"), is(expected2));
         assertThat(sqlRegistry.findSql("KEY3"), is(expected3));
