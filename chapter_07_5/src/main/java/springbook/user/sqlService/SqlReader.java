0d806fba2512407ed108b065c79b7815034e0d0d
bgpark <bgpark82@gmail.com>
2021-07-31 21:11:25 +0900

    feat: chapter_07_5

---
+++ chapter_07_5/src/main/java/springbook/user/sqlService/SqlReader.java
@@ -0,0 +1,6 @@
+package springbook.user.sqlService;
+
+public interface SqlReader {
+
+    void read(SqlRegistry sqlRegistry);
+}
---
+++ chapter_07_5/src/test/java/springbook/leanringtest/spring/oxm/OxmTest.java
@@ -0,0 +1,42 @@
+package springbook.leanringtest.spring.oxm;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.oxm.Unmarshaller;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import springbook.user.sqlService.jaxb.SqlType;
+import springbook.user.sqlService.jaxb.Sqlmap;
+
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+import java.io.IOException;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "/oxm-applicationContext.xml")
+public class OxmTest {
+
+    @Autowired
+    Unmarshaller unmarshaller;
+
+    @Test
+    public void unmarshallSqlMap() throws IOException {
+        Source xmlSource = new StreamSource(getClass().getResourceAsStream("/sqlmap.xml"));
+        Sqlmap sqlmap = (Sqlmap) unmarshaller.unmarshal(xmlSource);
+
+        List<SqlType> sqlList = sqlmap.getSql();
+
+        assertThat(sqlList.size(), is(3));
+        assertThat(sqlList.get(0).getKey(), is("add"));
+        assertThat(sqlList.get(0).getValue(), is("insert"));
+        assertThat(sqlList.get(1).getKey(), is("get"));
+        assertThat(sqlList.get(1).getValue(), is("select"));
+        assertThat(sqlList.get(2).getKey(), is("delete"));
+        assertThat(sqlList.get(2).getValue(), is("delete"));
+    }
+}
