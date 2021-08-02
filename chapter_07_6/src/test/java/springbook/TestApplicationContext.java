38b66dc3d88958127a5a5b443b9eb5fcf7c9f075
bgpark <bgpark82@gmail.com>
2021-08-02 13:02:49 +0900

    refactoring: @Bean으로 정의한 dataSource 빈

--- chapter_07_6/src/test/java/springbook/TestApplicationContext.java
+++ chapter_07_6/src/test/java/springbook/TestApplicationContext.java
@@ -1,9 +1,26 @@
 package springbook;
 
+import com.mysql.jdbc.Driver;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+
+import javax.sql.DataSource;
 
 @Configuration
 @ImportResource("/test-applicationContext.xml")
 public class TestApplicationContext {
+
+    @Bean
+    public DataSource dataSource() {
+        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+
+        dataSource.setDriverClass(Driver.class);
+        dataSource.setUrl("jdbc:mysql://localhost:3306/toby");
+        dataSource.setUsername("toby");
+        dataSource.setPassword("toby");
+
+        return dataSource;
+    }
 }
