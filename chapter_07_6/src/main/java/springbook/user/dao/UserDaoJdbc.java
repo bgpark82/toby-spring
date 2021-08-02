8801d629be480269f09ef75311976919029ff7be
bgpark <bgpark82@gmail.com>
2021-08-02 22:39:21 +0900

    refactoring: UserDaoJdbc를 @Repository로 변경

--- chapter_07_6/src/main/java/springbook/user/dao/UserDaoJdbc.java
+++ chapter_07_6/src/main/java/springbook/user/dao/UserDaoJdbc.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 import springbook.user.domain.Level;
 import springbook.user.domain.User;
 import springbook.user.sqlService.SqlService;
@@ -14,7 +14,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
-@Component
+@Repository
 public class UserDaoJdbc implements UserDao{
 
     private JdbcTemplate jdbcTemplate;
