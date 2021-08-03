8c85457b723995e215abaf8f433a2eef7725d341
bgpark <bgpark82@gmail.com>
2021-08-03 22:11:12 +0900

    refactoring: @Profile 지정

--- chapter_07_6/src/test/java/springbook/TestAppContext.java
+++ chapter_07_6/src/test/java/springbook/TestAppContext.java
@@ -2,6 +2,7 @@ package springbook;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.mail.MailSender;
 import springbook.user.service.DummyMailSender;
 import springbook.user.service.UserService;
@@ -9,6 +10,7 @@ import springbook.user.service.UserService;
 import static springbook.user.service.UserServiceTest.TestUserServiceImpl;
 
 @Configuration
+@Profile("test")
 public class TestAppContext {
 
     @Bean
