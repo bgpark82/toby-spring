4a7581e3eea85cf69c9fba8412639c266385100a
bgpark <bgpark82@gmail.com>
2021-08-03 23:50:18 +0900

    refactoring: TestAppContext와 ProductionAppContext를 중첩 클래스로 만든 AppContext

--- chapter_07_6/src/test/java/springbook/AppContext.java
+++ chapter_07_6/src/test/java/springbook/AppContext.java
@@ -1,14 +1,16 @@
 package springbook;
 
 import com.mysql.jdbc.Driver;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.*;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import springbook.user.service.DummyMailSender;
+import springbook.user.service.UserService;
+import springbook.user.service.UserServiceTest;
 
 import javax.sql.DataSource;
 
@@ -35,5 +37,32 @@ public class AppContext {
         DataSourceTransactionManager tm = new DataSourceTransactionManager();
         tm.setDataSource(dataSource());
         return tm;
+    }
+
+    @Configuration
+    @Profile("production")
+    public static class ProductionAppContext {
+
+        @Bean
+        public MailSender mailSender() {
+            JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+            mailSender.setHost("localhost");
+            return mailSender;
+        }
+    }
+
+    @Configuration
+    @Profile("test")
+    public static class TestAppContext {
+
+        @Bean
+        public UserService testUserService() {
+            return new UserServiceTest.TestUserServiceImpl();
+        }
+
+        @Bean
+        public MailSender mailSender() {
+            return new DummyMailSender();
+        }
     }
 }
